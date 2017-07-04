492285c223d6b1a9470b197836896007f6bc1540
SHRIRAM BABU <shrirambabu18@gmail.com>
2017-07-04 16:07:38 +0530

    Create config.php

---
+++ Task 3/Basic mode/login/config.php
@@ -0,0 +1,10 @@
+<?php
+	define('DB_SERVER', 'localhost');
+	define('DB_USERNAME', 'DeltaUser');
+	define('DB_PASSWORD', 'password');
+	define('DB_DATABASE', 'Delta_Task_3');
+	$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,9000);
+	if(  $conn->connect_error ) {
+		die('Could not connect: ' . $conn->connect_error);
+	}
+?>
