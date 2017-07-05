b367bab3957a42fa7b673318a242fc15bb3bee17
SHRIRAM BABU <shrirambabu18@gmail.com>
2017-07-05 23:06:38 +0530

    Update config.php

--- Task 3/Basic mode/login/config.php
+++ Task 3/Basic mode/login/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'DeltaUser');
+	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', 'password');
 	define('DB_DATABASE', 'Delta_Task_3');
 	$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE,9000);
